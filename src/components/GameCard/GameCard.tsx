97a6dc7cd1b14e917073b56c5e0ef03b37886030
Mike Filicetti <filicetti.michael@gmail.com>
2020-02-27 10:52:55 -0800

    GameCard Component commenting complete

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -5,9 +5,16 @@ import { Dustbin} from '../Dustbin'
 import { Box } from '../Box'
 import update from 'immutability-helper'
 
+// GameCard functional component: The GameCard acts as a styled  
+// container to hold each game gard. Users can carousel through
+// all the game cards within the set provided in the JSON object
+// or API data being consumed 
 const GameCard: React.FC<GameCardState> = ({ players }) => {
   const boxesArr: any[] = []
   
+  // Method used to push player objects from the JSON object or
+  // API data into an array, in randomized order, for display on 
+  // each game card
   const GenerateOptions = () => Object.values(players).map(player => {
     boxesArr.push({
       name: player.fullName,
@@ -18,6 +25,8 @@ const GameCard: React.FC<GameCardState> = ({ players }) => {
     return boxesArr.sort(() => 0.5 - Math.random())
   })
   
+  // Method used to first generate the possible answers for each
+  // game card, then determine the desired answer
   const GenerateAnswer = () => {
     GenerateOptions()
     let int = Math.ceil((Math.random() * 10) / 2)
@@ -25,17 +34,25 @@ const GameCard: React.FC<GameCardState> = ({ players }) => {
     return answer
   }
   
+  // Setup hook using DustinState from our Component Interface
+  // and determine desired answer for each game card
   const [dustbins, setDustbins] = useState<DustbinState[]>([
     { accepts: ['any'], lastDroppedItem: null, profile: GenerateAnswer() }
   ])
 
+  // Setup hook using BoxState from our Component Interface
+  // and select five possible options for each card
   const [boxes] = useState<BoxState[]>(boxesArr.slice(0, 5))
   const [droppedBoxNames, setDroppedBoxNames] = useState<string[]>([])
 
+  // Method that accepts a Box name and confirms if it has yet
+  // been dropped into the card's Dustbin
   const isDropped = (boxName: string) => {
     return droppedBoxNames.indexOf(boxName) > -1
   }
 
+  // Callback method used to pass off the data from the Box
+  // being dropped to the Dustbin it's being dropped into
   const handleDrop = useCallback(
     (index: number, item: { name: string, uid: any, profilePicture: any }) => {
       const { name } = item
